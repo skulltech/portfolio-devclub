78ae021826de8b326d3b944757260c8ffae8310e
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-03 02:18:06 +0530

    Make models Post and Tag in blog.models

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, null=True, blank=True, default='')
+
+    def __str__(self):
+        return self.name
