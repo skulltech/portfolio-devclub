5aec895207c9c9a1b269e3415851e46ea4c07b8b
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-03 21:15:24 +0530

    Implemented crispy forms with the contact app.

--- portfolio/contact/forms.py
+++ portfolio/contact/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from django.urls import reverse
 from .models import ContactMessage
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         exclude = ['submit_date']
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'contactForm'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('contact')
+
+        self.helper.add_input(Submit('submit', 'Send Message'))
--- portfolio/contact/urls.py
+++ portfolio/contact/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.contact, name='posts_list')
+    url(r'^$', views.contact, name='contact')
 ]
