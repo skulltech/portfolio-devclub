7137161cde4743517b08918fcb9903ef42238346
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-04 16:54:15 +0530

    Added basic files for app projects. Made the home page.

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
-    url(r'^contact/', include('contact.urls'))
+    url(r'^contact/', include('contact.urls')),
+    url(r'', include('projects.urls'))
 ]
